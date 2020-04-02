67fac67e98be301f8031d74ac9e84f1005635fb8
Brenda Matias <brenda.matias@mesa.com.br>
2020-04-01 23:34:24 -0300

    feat: migration and appointment model

--- src/database/index.js
+++ src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
@@ -17,7 +18,9 @@ class Database {
 
     models
       .map((model) => model.init(this.connection))
-      .map((model) => model && model.associate(this.connection.models));
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
   }
 }
 
