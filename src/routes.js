fa4518e11e7834d2d3ed08e6c06899ca2fe17938
Brenda Matias <brenda.matias@mesa.com.br>
2020-04-01 23:56:28 -0300

    feat: listing user appointments

--- src/routes.js
+++ src/routes.js
@@ -24,5 +24,6 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
 
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 export default routes;
